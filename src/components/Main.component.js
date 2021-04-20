061bd3ab8d301941fc82ddcd85b6703914300f76
AnastasiiaSyng <nastya491@gmail.com>
2021-04-20 19:10:03 +0100

    added styles

--- src/components/Main.component.js
+++ src/components/Main.component.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import './Main.styles.css'
 
 
 const MainComponent = () => {
@@ -81,30 +82,45 @@ const MainComponent = () => {
     const limetedResult = result.slice(offset, limit);
 
     return (
-        <div>
-            <form onSubmit={getResult}>
-                <input type='text' name='input'></input>
-                <button type='submit'>submit</button>
-            </form>
-            <input 
-            type='text'
-            onChange={filterList}
-            >
-            </input>
-            <select value={defaultLimit} onChange={changeLimit}>
-                {options.map((option) => (
-                <option value={option.value}>{option.label}</option>
-                ))}
-          </select>
+        <div className='container'>
+            <div className='input_container'>
+                <form onSubmit={getResult}>
+                    <input type='text' name='input'></input>
+                    <button type='submit'>submit</button>
+                </form>
+                <div>
+                <span>Show</span> <select value={defaultLimit} onChange={changeLimit}>
+                    {options.map((option) => (
+                    <option value={option.value}>{option.label}</option>
+                    ))}
+                </select><span>entries</span><span>  </span>
+                <input 
+                    placeholder='search'
+                    type='text'
+                    onChange={filterList}
+                />
+                </div>
+            </div>
             <div> 
+                <div className='table_name'>
+                    <p>RECORD</p>
+                    <p>VALUE</p>
+                </div>
+                
                 {limetedResult.map(item => {
-                    return (<li key={uuidv4()}>{item.value}</li>)
+                    return (<div key={uuidv4()} className='result_container'>
+                                    <div>{item.name}</div>
+                                    <div>{item.value}</div>
+                            </div>)
                 })}
             </div>
-            <button onClick={showNext}>Next</button>
-            <button onClick={showPrev}>Revious</button>
-
-            <p>Showing 1 to {defaultLimit} of {result.length} entrires</p>
+            <div className='scroll_container'>
+                <p>Showing 1 to {defaultLimit} of {result.length} entrires</p>
+                <div>
+                    <button onClick={showPrev}>Revious</button>
+                    <button onClick={showNext}>Next</button>
+                </div>
+            </div>
         </div>
     )
 }
